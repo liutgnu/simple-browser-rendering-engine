dc2ed683af38c9fd19d578dcd8c48506b5bc15c5
liutgnu <liutgnu@gmail.com>
2020-08-02 21:24:02 +0800

    pretty print for dom tree

--- utils/dom.h
+++ utils/dom.h
@@ -13,6 +13,14 @@ enum DomNodeType {
     TEXT
 };
 
+bool in_list(vector<int> *list, int val) {
+    for (vector<int>::iterator it = list->begin(); it != list->end(); ++it) {
+        if (val == *it)
+            return true;
+    }
+    return false;
+}
+
 class DomNode {
 
     public:
@@ -27,9 +35,18 @@ class DomNode {
     DomNode(string& tag_name, map<string, string>& attributes, vector<DomNode>& child_list):
         tag_name(tag_name), attributes(attributes), type(ELEMENT), child_list(child_list) {}
     
-    void print(int32_t depth) {
+    void print(int32_t depth, bool is_last_child, vector<int32_t>& list) {
         for (int32_t i = 0; i < depth; ++i) {
-            cout << "  ";
+            if (is_last_child && i == depth - 1) {
+                std::cout << "\xE2\x94\x94\xE2\x94\x80";
+            } else if (!is_last_child && i == depth - 1) {
+                std::cout << "\xE2\x94\x9c\xE2\x94\x80";
+            }
+            else if (in_list(&list, i)) {
+                std::cout << "\xE2\x94\x82 ";
+            }
+            else 
+                std::cout << "  ";
         }
 
         if (type == TEXT) {
@@ -40,22 +57,30 @@ class DomNode {
     }
 };
 
-void print_dom_node(DomNode& root, int32_t depth) {
+void print_dom_node(DomNode& root, int32_t depth, bool is_last_child, vector<int32_t>& list) {
     ++depth;
-    root.print(depth);
+
+    root.print(depth, is_last_child, list);
     for (vector<DomNode>::iterator it = root.child_list.begin();
         it != root.child_list.end(); ++it) {
-        print_dom_node(*it, depth);
+        if (it == root.child_list.begin()) {
+            list.push_back(depth);
+        }
+        if (it == root.child_list.end() - 1) {
+            list.pop_back();
+        }
+        print_dom_node(*it, depth, it == root.child_list.end() - 1, list);
     }
     --depth;
 }
 
 void print_dom_list(vector<DomNode> root_list) {
     static int32_t depth = 0;
+    static vector<int32_t> list;
 
     for (vector<DomNode>::iterator it = root_list.begin();
         it != root_list.end(); ++it) {
-        print_dom_node(*it, depth);
+        print_dom_node(*it, depth, it == root_list.end() - 1, list);
     }
 }
 
