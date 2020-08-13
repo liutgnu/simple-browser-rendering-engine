a4c72cc41969ecd2c7f08328b62cce0fc97ed069
liutao <taobliu@thoughtworks.com>
2020-08-13 23:24:18 +0800

    in develop

--- layout/layout_node.h
+++ layout/layout_node.h
@@ -55,10 +55,9 @@ LayoutNode trans_style_dom(const StyleDomNode& node) {
 }
 
 
-
-void calculate_node_width(const StyleDomNode& node, const Box& container_box) {
+void calculate_block_node_width(LayoutNode& node, const Box& container_box) {
     Value zero_length(make_tuple(0.0, "px"));
-    Value auto_keyword("auto");
+    Value auto_value("auto");
     float width_ma_L, width_ma_R, width_ma_; 
 
     const Value *margin_left = find_in_map_or_default(node.property_map, 
@@ -85,31 +84,83 @@ void calculate_node_width(const StyleDomNode& node, const Box& container_box) {
     
     vector<float> width_list(7);
     transform(width_list_origin.cbegin(), width_list_origin.cend(), width_list.begin(), 
-        [](const Value& v) -> float {
-            if (v.type == LENGTH && v.Length.unit == "px") {
-                return v.Length.data;
-            }
-            return 0.0;
+        [](const Value& v) {
+            return v.to_px();
     });
-    float total = accumulate(width_list.cbegin(), width_list.cend(), 0, [](float a, float b) -> float {return a + b;});
+    float total = accumulate(width_list.cbegin(), width_list.cend(), 0, 
+        [](float a, float b) {return a + b;});
     float overflow = total - container_box.content.width;
 
     tuple<bool, bool, bool> result = make_tuple(
-        auto_keyword == *margin_left, auto_keyword == *width, auto_keyword == *margin_right);
+        auto_value == *margin_left, auto_value == *width, auto_value == *margin_right);
 
-    if (result == make_tuple(false, false, false)) {
-        width_ma_R = margin_right->to_px() + overflow;
-    } else if (result == make_tuple(false, false, true)) {
+    width_ma_L = margin_left->to_px();
+    width_ma_R = margin_right->to_px();
+    width_ma_ = width->to_px();
+
+    if (make_tuple(get<0>(result), get<1>(result)) == make_tuple(false, false)) {  // (false, false, ~)
         width_ma_R = margin_right->to_px() + overflow;
     } else if (result == make_tuple(true, false, false)) {
-        width_ma_L = margin_right->to_px() + overflow;
+        width_ma_L = margin_left->to_px() + overflow;
     } else if (result == make_tuple(true, false, true)) {
-        
+        width_ma_R = overflow / 2.0;
+        width_ma_L = overflow / 2.0;
+    } else if (get<1>(result) == true) {  // (~, true, ~)
+        width_ma_L = auto_value == *margin_left ? 0 : margin_left->Length.data;
+        width_ma_R = auto_value == *margin_right ? 0 : margin_right->Length.data;
+        if (overflow >= 0) {
+            width_ma_ = overflow;
+        } else {
+            width_ma_ = 0;
+            width_ma_R += overflow;
+        }
+    } else {  // never reach here
+        assert(false);
     }
+
+    node.box.content.width = width_ma_;
+    node.box.padding.left = padding_left->to_px();
+    node.box.padding.right = padding_right->to_px();
+    node.box.border.left = border_left->to_px();
+    node.box.border.right = border_right->to_px();
+    node.box.margin.left = width_ma_L;
+    node.box.margin.right = width_ma_R;
 }
 
-void calculate_node_layout(const StyleDomNode& node, const Box& container_box) {
+void calculate_block_node_position(LayoutNode& node, const Box& container_box) {
+    Value zero_length(make_tuple(0.0, "px")); 
+    node.box.margin.top = (node.property_map,
+        make_type_vector<string>(2, "margin-top", "margin"), &zero_length)->to_px();
+    node.box.margin.bottom = (node.property_map,
+        make_type_vector<string>(2, "margin-bottom", "margin"), &zero_length)->to_px();
+    node.box.border.top = (node.property_map,
+        make_type_vector<string>(2, "border-top", "border"), &zero_length)->to_px();
+    node.box.border.bottom = (node.property_map,
+        make_type_vector<string>(2, "border-bottom", "border"), &zero_length)->to_px();
+    node.box.padding.top = (node.property_map,
+        make_type_vector<string>(2, "padding-top", "padding"), &zero_length)->to_px();
+    node.box.padding.bottom = (node.property_map,
+        make_type_vector<string>(2, "padding-bottom", "padding"), &zero_length)->to_px();
+    node.box.content.x = container_box.content.x + node.box.margin.left + 
+        node.box.border.left + node.box.padding.left;
+    node.box.content.y = container_box.content.y + node.box.margin.top + 
+        node.box.border.top + node.box.padding.top;
+}
+
+void calculate_block_children(LayoutNode& node) {
 
 }
+
+// void calculate_block_node_layout(LayoutNode& node, const Box& container_box) {
+//     switch (node.type) {
+//         case BLOCK:
+//             return calculate_block_node_width(node, container_box);
+//         case INLINE:
+//         case ANONYMOUS:
+//             return;
+//         default:
+//             assert(false);
+//     }
+// }
 
 }
